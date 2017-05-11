e4435b482910699b6a3c2cce21faff1a254cb48b
alexmady <alexmady@gmail.com>
2017-05-11 14:36:40 +0100

    smooth working version

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild, NgZone } from '@angular/core';
-import { Content, NavParams, NavController, Platform } from 'ionic-angular';
+import { Component, ViewChild, Renderer } from '@angular/core';
+import { Content, NavParams, NavController } from 'ionic-angular';
 import { Keyboard } from 'ionic-native';
 
 @Component({
@@ -11,16 +11,17 @@ export class HelloIonicPage {
 
   @ViewChild(Content) content: Content;
   private inputElement;
-  private millis = 100;
+  private millis = 200;
   private textareaHeight;
-  private isTextareaFocused = false;
-  private noHideAdjust = false;
   private scrollContentElelment: any;
   private footerElement: any;
   private initialTextAreaHeight;
   private user;
   private keyboardHideSub;
   private keybaordShowSub;
+
+  private fakeBGVisible = true;
+
   public message = "";
   public messages: any[] = [
     {
@@ -47,26 +48,34 @@ export class HelloIonicPage {
       position: 'right',
       body: 'fff'
     },
+    {
+      position: 'right',
+      body: 'fff'
+    },
+    {
+      position: 'right',
+      body: 'fff'
+    },
+    {
+      position: 'right',
+      body: 'fff'
+    }
   ];
 
 
 
-  constructor(keybaord: Keyboard, public plt: Platform, private ngZone: NgZone, public navParams: NavParams, public navCtrl: NavController) {
+  constructor(keybaord: Keyboard, public renderer: Renderer, public navParams: NavParams, public navCtrl: NavController) {
 
     this.user = navParams.get('user');
 
   }
 
   footerTouchStart(event) {
-    console.log('preventing - footerTouchStart')
-    if (event.target.localName !== "textarea") {
-      //event.preventDefault();
-    }
-  }
+    console.log('footerTouchStart: ', event.type, event.target.localName, '...')
 
-  ionInputTouchStart(evt) {
-    if (this.isTextareaFocused) {
-      //evt.preventDefault();
+    if (event.target.localName !== "textarea") {
+      event.preventDefault();
+      console.log('preventing')
     }
   }
 
@@ -79,126 +88,108 @@ export class HelloIonicPage {
       this.textareaHeight = newHeight;
       console.log('new text area height', this.textareaHeight);
       let newNumber = Number(this.scrollContentElelment.style.marginBottom.replace('px', '')) + diffHeight;
-      this.scrollContentElelment.style.marginBottom = newNumber + 'px';
+      
+      let top = newNumber + 'px';
+      console.log('textAreaChange', 'setting top to', top);
+      //this.scrollContentElelment.style.marginBottom = newNumber + 'px';
+      this.renderer.setElementStyle(this.scrollContentElelment, 'marginBottom', top );
+      // this.content.resize();
+      // if (this.contentBottom()) {
       this.updateScroll('textAreaChange');
+      // }
     }
   }
 
-  back($event: Event) {
+  back(event: Event) {
+
+    this.fakeBGVisible = false;
 
     this.inputElement.blur();
-
-
-    //this.inputElement.onblur = null;
     this.navCtrl.pop().then(() => {
       this.keyboardHideSub.unsubscribe();
       this.keybaordShowSub.unsubscribe();
     });
   }
 
+  ionScrollEnd(){
+    console.log('reaced ion scroll end, about to hide Keyboard');
+  }
+
   ionViewDidLoad() {
 
     this.keyboardHideSub = Keyboard.onKeyboardHide().subscribe(() => {
 
-      console.log('keybaordHide');
-      // if (!this.noHideAdjust) {
+      console.log('keyboard Hide');
       let newHeight = this.textareaHeight - this.initialTextAreaHeight + 44;
-      let newHeightStr = newHeight + 'px';
-      this.scrollContentElelment.style.marginBottom = newHeightStr;
-      this.footerElement.style.marginBottom = '0px';
-      this.updateScroll('keybaordHide');
-      // }
+      let top = newHeight + 'px';
 
+      this.renderer.setElementStyle(this.scrollContentElelment, 'marginBottom', top);
+      console.log('setting top to 0px')
+      this.renderer.setElementStyle(this.footerElement, 'marginBottom', '0px')
     });
 
     this.keybaordShowSub = Keyboard.onKeyboardShow().subscribe((e) => {
 
-      console.log('keybaordShow');
-      if (!this.isTextareaFocused) {
-        this.isTextareaFocused = true;
-        this.noHideAdjust = false;
-        let newHeight = (e['keyboardHeight'] + 44) + this.textareaHeight - this.initialTextAreaHeight;
-        this.scrollContentElelment.style.marginBottom = newHeight + 'px';
-        this.footerElement.style.marginBottom = e['keyboardHeight'] + 'px';
+      console.log('keyboard Show');
+      let newHeight = (e['keyboardHeight']) + this.textareaHeight - this.initialTextAreaHeight;
+      let top =  newHeight + 44 + 'px';
+      this.renderer.setElementStyle(this.scrollContentElelment, 'marginBottom',top );
+      console.log('top', top);
 
-        setTimeout(() => {
-          this.updateScroll('keybaordShow');
-        }, 100)
-      }
-
+      this.renderer.setElementStyle(this.footerElement, 'marginBottom', e['keyboardHeight'] + 'px');
+      this.updateScroll('keybaord show');
     });
 
-    let page: any = document.getElementsByTagName('hello-ionic-page');
-    this.scrollContentElelment = page[0].children[1].children[1];
-    this.footerElement = document.getElementsByClassName('footer')[0];
-    this.inputElement = document.getElementsByTagName('textarea')[0];
+    this.scrollContentElelment = this.content.getScrollElement();
+
+    console.log('scroll element', this.scrollContentElelment)
+
+
+    this.footerElement = document.getElementsByTagName('hello-ionic-page')[0].getElementsByTagName('ion-footer')[0];
+    this.inputElement = document.getElementsByTagName('hello-ionic-page')[0].getElementsByTagName('textarea')[0];
 
     this.footerElement.style.cssText = this.footerElement.style.cssText + "transition: all " + this.millis + "ms; -webkit-transition: all " +
       this.millis + "ms; -webkit-transition-timing-function: ease-out; transition-timing-function: ease-out;"
 
-    // let sc: any = document.getElementsByClassName('scroll-content')[1];
-    // sc.style.cssText = sc.style.cssText + "transition: all " + this.millis + "ms; -webkit-transition: all " +
-    //   this.millis + "ms; -webkit-transition-timing-function: ease-out; transition-timing-function: ease-out;"
+    this.scrollContentElelment.style.cssText = this.scrollContentElelment.style.cssText + "transition: all " + this.millis + "ms; -webkit-transition: all " +
+      this.millis + "ms; -webkit-transition-timing-function: ease-out; transition-timing-function: ease-out;"
 
     this.textareaHeight = Number(this.inputElement.style.height.replace('px', ''));
     this.initialTextAreaHeight = this.textareaHeight;
 
-    // this.inputElement.onfocus = (evt: Event) => {
-    //   this.inputElement.onblur = (event: Event) => {
-    //     event.preventDefault();
-    //     this.inputElement.focus();
-    //   };
-    // }
+    this.updateScroll('load')
+
   }
 
   contentMouseDown(event) {
-
-    console.log('blurring input element');
+    //console.log('blurring input element :- > event type:', event.type);
     this.inputElement.blur();
-    this.isTextareaFocused = false;
-    if (this.inputElement.onblur) {
-      this.inputElement.onblur = null;
-    }
   }
 
   touchSendButton(event: Event) {
+    //console.log('touchSendButton, event type:', event.type);
     event.preventDefault();
-    this.noHideAdjust = true;
     this.sendMessage();
   }
 
   sendMessage() {
 
-    this.ngZone.run(() => {
-
-      this.messages.push({
-        position: 'left',
-        body: this.message
-      });
-
-    })
+    this.messages.push({
+      position: 'left',
+      body: this.message
+    });
 
     this.message = "";
-    //this.textareaHeight = this.initialTextAreaHeight;
 
-    //if (this.plt.is('core') || this.plt.is('mobileweb')) {
     let currentHeight = this.scrollContentElelment.style.marginBottom.replace('px', '');
     let newHeight = currentHeight - this.textareaHeight + this.initialTextAreaHeight;
-
-    console.log('-----------------------');
-    console.log('current height ', currentHeight);
-    console.log('textareaHeight ', this.textareaHeight);
-    console.log('initialTextAreaHeight ', this.initialTextAreaHeight);
-    console.log('new height ', newHeight);
-    console.log('-----------------------');
-
-    this.scrollContentElelment.style.marginBottom = newHeight + 'px';
+    let top =  newHeight + 'px';
+    this.renderer.setElementStyle(this.scrollContentElelment, 'marginBottom', top);
+    console.log('send message, top', top)
     this.updateScroll('sendMessage');
     this.textareaHeight = this.initialTextAreaHeight;
 
-    //  this.scrollContentElelment.style.marginBottom = '44px';
-    //}
-
+    //DUMMY response message
     setTimeout(() => {
       this.messages.push({
         position: 'right',
@@ -214,6 +205,6 @@ export class HelloIonicPage {
     setTimeout(() => {
       console.log('updating scroll -->', from)
       this.content.scrollToBottom();
-    }, 100);
+    }, 300);
   }
 }
