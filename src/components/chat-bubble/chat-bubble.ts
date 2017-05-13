193362e71dce4a09effbdd88494b8df14bcac6c6
alexmady <alexmady@gmail.com>
2017-05-14 00:20:07 +0100

    add basic image functionality

--- src/components/chat-bubble/chat-bubble.ts
+++ src/components/chat-bubble/chat-bubble.ts
@@ -6,9 +6,15 @@ import {Component} from '@angular/core';
     `
       <div class="chatBubble">
         <!-- <div class="profile-pic {{msg.position}}"></div> -->
-        <div class="chat-bubble {{msg.position}}">
+        <div *ngIf="msg.body" class="chat-bubble {{msg.position}}">
           <div class="message">{{msg.body}}</div>
           <div class="message-detail">
+              <span>{{msg.timestamp| date: "HH:mm"}}</span>
+          </div>
+        </div>
+         <div *ngIf="msg.img" class="chat-bubble-img {{msg.position}}">
+          <img src="{{msg.img}}">
+          <div class="message-detail-img">
               <span>{{msg.timestamp| date: "HH:mm"}}</span>
           </div>
         </div>
